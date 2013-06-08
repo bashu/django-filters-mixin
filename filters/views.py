b6ed217b5d2a065626d76a569713ca169275e61e
Basil Shubin <basil.shubin@gmail.com>
2013-06-08 12:18:36 +0700

    initial commit

---
+++ filters/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django_filters import views as filters_views
+
+__all__ = ['FilterMixin']
+
+
+class FilterMixin(filters_views.FilterMixin):
+
+    def get_filterset_kwargs(self, filterset_class):
+        kwargs = super(FilterMixin, self).get_filterset_kwargs(filterset_class)
+        if kwargs['data'] is not None and 'page' in kwargs['data']:
+            data = kwargs['data'].copy() ; del data['page']
+            kwargs['data'] = data
+        return kwargs
---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-filters',
+    version='0.1',
+    packages=['filters'],
+    include_package_data=True,
+    license='BSD License',
+    description='Put short description here...',
+    long_description=README,
+    url='http://github.com/bashu/django-filters',
+    author='Basil Shubin',
+    author_email='basil.shubin@gmail.com',
+    install_requires=[
+        'django-filter',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    zip_safe=False,
+)

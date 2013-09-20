25831e984c3b3dead819b29eecc8b4558d7b4f09
Basil Shubin <basil.shubin@gmail.com>
2013-09-20 12:58:03 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-filters',
-    version='0.1',
+    name='django-filters-mixin',
+    version='0.1.1',
     packages=['filters'],
     include_package_data=True,
     license='BSD License',
@@ -18,7 +18,7 @@ setup(
     author='Basil Shubin',
     author_email='basil.shubin@gmail.com',
     install_requires=[
-        'django-filter',
+        'django-filter==0.7',
     ],
     classifiers=[
         'Environment :: Web Environment',

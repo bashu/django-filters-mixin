dd0fc59d004643e324fb3df728fc8e967a8e9bda
Basil Shubin <basil.shubin@gmail.com>
2014-10-09 12:23:17 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-filters-mixin',
-    version='0.1.1',
+    version='0.1.2',
     packages=['filters'],
     include_package_data=True,
     license='BSD License',

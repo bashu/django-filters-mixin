5e3f971baf8003e820278d83b6adf21c9230bd0f
bashu <basil.shubin@gmail.com>
2016-01-11 12:39:35 +0500

    configuree universal wheel building

--- setup.py
+++ setup.py
@@ -1,25 +1,50 @@
+#!/usr/bin/env python
+
 import os
+import re
+import sys
+import codecs
+
 from setuptools import setup, find_packages
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+def read(*parts):
+    file_path = os.path.join(os.path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return str(version_match.group(1))
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='django-filters-mixin',
-    version='0.1.2',
-    packages=find_packages(exclude=['example']),
-    include_package_data=True,
+    version=find_version('filters', '__init__.py'),
     license='BSD License',
-    description='django-filter meets django-pagination',
-    long_description=README,
-    url='http://github.com/bashu/django-filters',
-    author='Basil Shubin',
-    author_email='basil.shubin@gmail.com',
+
     install_requires=[
         'django-filter>=0.8',
     ],
+    requires=[
+        'Django (>=1.4)',
+    ],
+
+    description='django-filter meets django-pagination',
+    long_description=read('README.rst'),
+
+    author='Basil Shubin',
+    author_email='basil.shubin@gmail.com',
+
+    url='http://github.com/bashu/django-filters-mixin',
+    download_url='https://github.com/bashu/django-filters-mixin/zipball/master',
+
+    packages=find_packages(exclude=('example*',)),
+    include_package_data=True,
+
+    zip_safe=False,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -34,6 +59,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
-    zip_safe=False,
+    ]
 )
